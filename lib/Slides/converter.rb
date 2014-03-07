9b1ca7a355f9455dc3e53aae83afc3b4cdf4e87e
Assad Mahmood Qazi <assadvirgo@gmail.com>
2014-03-07 15:45:55 +0500

    Interpolation instead of string concat
    Improvements in param checking

--- lib/Slides/converter.rb
+++ lib/Slides/converter.rb
@@ -1,11 +1,10 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 module Aspose
   module Cloud
     module Slides
       class Converter
         def initialize filename
           @filename = filename
+          raise 'Base file not specified.' if @filename.empty?
         end    
 
 =begin
@@ -17,28 +16,12 @@ module Aspose
 =end    
         def convert_to_image slide_number, image_format, width, height
           begin
-        
-            if @filename == ''
-              raise 'filename not specified'
-            end
-        
-            if slide_number == ''
-              raise 'slide number not specified'
-            end
-        
-            if image_format == ''
-              raise 'image format not specified'
-            end
-        
-            if width == ''
-              raise 'width not specified'
-            end
-        
-            if height == ''
-              raise 'height not specified'
-            end
-                
-            str_uri = $product_uri + '/slides/' + @filename + '/slides/' + slide_number.to_s + '?format=' + image_format +'&width=' + width.to_s + '&height=' + height.to_s
+            raise 'Slide number not specified.' if slide_number.nil?
+            raise 'Image format not specified.' if image_format.empty?
+            raise 'Width not specified.' if width.nil?
+            raise 'Height not specified.' if height.nil?
+
+            str_uri = "#{$product_uri}/slides/#{@filename}/slides/#{slide_number}?format=#{image_format}&width=#{width}&height=#{height}"
         
             str_signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)        
             response_stream = RestClient.get(str_signed_uri, {:accept=>'application/json'})  
@@ -46,14 +29,13 @@ module Aspose
             valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
         
             if valid_output == ''
-              output_path = $out_put_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '.' + image_format
+              output_path = "#{$out_put_location}#{Aspose::Cloud::Common::Utils.get_filename(@filename)}.#{image_format}"
               Aspose::Cloud::Common::Utils.save_file(response_stream,output_path)
               return ''
             else
               return valid_output
             end
-        
-        
+
           rescue Exception=>e
             print e
           end
@@ -62,44 +44,31 @@ module Aspose
 =begin
   convert a document to SaveFormat
 =end    
-        def convert save_format,slide_number, storage_name='', folder=''
+        def convert save_format, slide_number, storage_name='', folder=''
           begin
-        
-            if @filename == ''
-              raise 'filename not specified'
-            end                
-        
-            if save_format == ''
-              raise 'save format not specified'
-            end
-                
-            if(storage_name.empty? == true)       
-              str_uri = $product_uri + '/slides/' + @filename + '/slides/' + slide_number.to_s + '?format=' + save_format
-            else
-              str_uri = $product_uri + '/slides/' + @filename + '/slides/' + slide_number.to_s + '?format=' + save_format + '&storage=' + storage_name + '&folder=' + folder
-            end
-        
+
+            raise 'Save format not specified.' if save_format.empty?
+
+            str_uri = "#{$product_uri}/slides/#{@filename}/slides/#{slide_number}?format=#{save_format}"
+            str_uri += "&storage=#{storage_name}&folder=#{folder}" unless storage_name.empty?
+
             str_signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)        
             response_stream = RestClient.get(str_signed_uri, {:accept=>'application/json'})  
         
             valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
         
             if valid_output == ''
-              output_path = $out_put_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '.' + save_format
+              output_path = "#{$out_put_location}#{Aspose::Cloud::Common::Utils.get_filename(@filename)}.#{save_format}"
               Aspose::Cloud::Common::Utils.save_file(response_stream,output_path)
               return ''
             else
               return valid_output
             end
-        
-        
           rescue Exception=>e
             print e
           end
-        end    
-    
+        end
       end
     end
-    
   end
 end
